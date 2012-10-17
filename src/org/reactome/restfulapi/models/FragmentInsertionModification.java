d2f5ef51b795ecb3614f4086ddb7e65a2a5cd37d
gwu <>
2012-10-17 17:15:11 +0000

    added XMLRootElement to classes so that they can be exposed

--- src/org/reactome/restfulapi/models/FragmentInsertionModification.java
+++ src/org/reactome/restfulapi/models/FragmentInsertionModification.java
@@ -4,10 +4,13 @@
  */
 package org.reactome.restfulapi.models;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * @author gwu
  *
  */
+@XmlRootElement
 public class FragmentInsertionModification extends FragmentModification {
     private Integer coordinate;
     
