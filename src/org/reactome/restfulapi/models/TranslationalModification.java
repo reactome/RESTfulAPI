56b5b402f36d3a15a3e73e1ad83e4de486579438
gwu <>
2012-10-17 17:15:11 +0000

    added XMLRootElement to classes so that they can be exposed

--- src/org/reactome/restfulapi/models/TranslationalModification.java
+++ src/org/reactome/restfulapi/models/TranslationalModification.java
@@ -4,10 +4,13 @@
  */
 package org.reactome.restfulapi.models;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * @author gwu
  *
  */
+@XmlRootElement
 public class TranslationalModification extends AbstractModifiedResidue {
     private Integer coordinate;
     private PsiMod psiMod;
