2ce8a77e84adf64540a30054320a5b705a53d418
gwu <>
2013-01-30 20:28:03 +0000

    added Deprecated annotation

--- src/org/reactome/restfulapi/models/ShellInstance.java
+++ src/org/reactome/restfulapi/models/ShellInstance.java
@@ -14,6 +14,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  *
  */
 @XmlRootElement(name="Insatnce")
+@Deprecated
 public class ShellInstance {
     private Long dbId;
     private String displayName;
