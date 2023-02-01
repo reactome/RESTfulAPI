cb3e2efa455c5018a6a604fcbe6bc11b0eaa2ae7
guanmingwu <guanmingwu@gmail.com>
2023-02-01 09:57:13 -0800

    Updated model for star system

--- src/org/reactome/restfulapi/models/MarkerReference.java
+++ src/org/reactome/restfulapi/models/MarkerReference.java
@@ -5,9 +5,18 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class MarkerReference extends ControlReference {
     
+    private List<Cell> cell;
     private List<EntityWithAccessionedSequence> marker;
     
     public MarkerReference() {
+    }
+
+    public List<Cell> getCell() {
+        return cell;
+    }
+
+    public void setCell(List<Cell> cell) {
+        this.cell = cell;
     }
 
     public List<EntityWithAccessionedSequence> getMarker() {
